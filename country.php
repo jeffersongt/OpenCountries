6d758cfac4ec199b9e5908e9a91fe35bc49481d0
Jefferson Guiot <jefferson.guiot@epitech.eu>
2021-09-27 15:47:25 +0200

    [FIX] : country search

--- country.php
+++ country.php
@@ -5,8 +5,15 @@ if (isset($_POST['country'])) {
 }
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://restcountries.eu/rest/v2/name/" . $country);
+curl_setopt($ch, CURLOPT_URL, "https://restcountries.com/v2/name/" . $country);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
+
+if (!$result) {
+    $err = curl_error($ch);
+    echo $err;
+    return;
+}
 curl_close($ch);
 
 $ret = json_decode(json_encode($result));
